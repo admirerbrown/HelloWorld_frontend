b61c0febb7c6c5e7e0b206c6f9785a78a09fe176
admirerbrown <admirerbrown@gmail.com>
2023-05-02 23:13:08 +0000

    APi commmunication successful

---
+++ src/components/DisplayGreeting.js
@@ -0,0 +1,20 @@
+import { useSelector } from 'react-redux';
+import React from 'react';
+
+
+const Greeting = () => {
+
+  const greetings = useSelector((state) => state.home.data);
+
+  if (greetings) {
+    return (
+      <div>
+        <h1>{greetings.message}</h1>
+      </div>
+    );
+  }
+
+};
+
+
+export default Greeting;
